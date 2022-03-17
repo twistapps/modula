19165f06bfa133087061b50f02046493f2ccd4a7
bloomstick <twistappscontact@gmail.com>
2022-03-17 03:30:42 +0300

    feat: new utilities (ModulaUtilities, TypeNames)
    TypeNames is created to associate Types with their Names.

---
+++ Scripts/Common/ModulaUtilities.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using UnityEngine;
+
+public static class ModulaUtilities
+{
+    //cache results of GetDerivedFrom because it's a pretty expensive method
+    private static readonly Dictionary<Type, Type[]> DerivativesDictionary = new Dictionary<Type, Type[]>();
+    
+    public static Type[] GetDerivedFrom<T>(Type[] ignored = null)
+    {
+        Type[] foundArr;
+        if (DerivativesDictionary.ContainsKey(typeof(T)))
+        {
+            // return cached result if GetDerivedFrom() has already been invoked before
+            foundArr = DerivativesDictionary[typeof(T)];
+        }
+        else
+        {
+            var found = (
+                from domainAssembly in AppDomain.CurrentDomain.GetAssemblies()
+                // alternative: from domainAssembly in domainAssembly.GetExportedTypes()
+                from assemblyType in domainAssembly.GetTypes()
+                where typeof(T).IsAssignableFrom(assemblyType)
+                // alternative: where assemblyType.IsSubclassOf(typeof(B))
+                // alternative: && ! assemblyType.IsAbstract
+                select assemblyType);
+
+            foundArr = found as Type[] ?? found.ToArray();
+            
+            DerivativesDictionary.Add(typeof(T), foundArr);
+        }
+
+        if (ignored != null)
+            foundArr = foundArr.Where(t => !ignored.Contains(t)).ToArray();
+
+        return foundArr;
+    }
+
+    public static Type[] ToTypesArray<TParentClass>(this string[] typeNames)
+    {
+        var types = new Type[typeNames.Length];
+        for (int i = 0; i < types.Length; i++)
+        {
+            types[i] = GetTypeByName<TParentClass>(typeNames[i]);
+        }
+        return types;
+    }
+
+    public static List<Type> ToTypesList<TParentClass>(this List<string> typeNames)
+    {
+        return ToTypesArray<TParentClass>(typeNames.ToArray()).ToList();
+    }
+
+    public static Type GetTypeByName<TParentClass>(string name)
+    {
+        if (name == null) return null;
+        var derivatives = GetDerivedFrom<TParentClass>();
+        return derivatives.FirstOrDefault(t => t.Name == name);
+    }
+
+    public static bool IsNullOrEmpty<T>(List<T> list)
+    {
+        if (list == null || list.Count == 0) return true;
+        return list.All(t => t == null);
+
+        //check if T is nullable
+        //if (Nullable.GetUnderlyingType(typeof(T)) != null)
+        //{
+        //    return list.All(t => t != null);
+        //}
+        //else
+        //{
+        //    return list.All(t => !t.GetType().GetDefaultMembers().Contains(t.GetType()));
+        //}
+    }
+
+    public static bool IsNullOrEmpty<T>(T[] arr)
+    {
+        if (arr == null || arr.Length == 0) return true;
+        return arr.All(t => t == null);
+    }
+
+    public static bool IsNullOrEmpty<T>(ReadOnlyCollection<T> col)
+    {
+        if (col == null || col.Count == 0) return true;
+        return col.All(t => t == null);
+    }
+}
---
+++ Scripts/Common/TypeNames.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+namespace Common
+{
+    /// <summary>
+    /// This class associates Types with their names in string format. This could be useful if you need to get Type by its name (GetType(string name))
+    /// All types should be derived from the same interface/class
+    /// </summary>
+    /// <typeparam name="TParentClass">the parent class/interface for all associated types</typeparam>
+    public class TypeNames<TParentClass>
+    {
+        // [ItemCanBeNull] public ReadOnlyCollection<string> Names => _names == null ? null : Array.AsReadOnly(_names); 
+        // [ItemCanBeNull] public ReadOnlyCollection<Type> Types => _types == null ? null : Array.AsReadOnly(_types);
+
+        public TypeNames(string[] typeNames, bool silentExceptions = true)
+        {
+            Names = typeNames;
+            Types = Names.ToTypesArray<TParentClass>();
+        }
+
+        public TypeNames(Type[] types, bool silentExceptions = true)
+        {
+            if (types == null)
+            {
+                Debug.LogWarning("types array should not be null while creating TypeNames instance");
+                return;
+            }
+
+            Types = types.Where(t =>
+                    t == null || t.IsSubclassOf(typeof(TParentClass)) || typeof(TParentClass).IsAssignableFrom(t))
+                .ToArray();
+            if (!silentExceptions && Types.Length != types.Length)
+                Debug.LogWarning("Passed types array contains elements that are not derived from " +
+                                 typeof(TParentClass).Name +
+                                 ". They were removed from this instance of TypeNames");
+
+            Names = types.Select(t => t?.Name).ToArray();
+        }
+
+        // public string[] names { 
+        //     get
+        //     {
+        //         if (IsNull()) return null;
+        //         string[] copy = new string[_names.Length];
+        //         Array.Copy(_names, copy, copy.Length);
+        //         return copy;
+        //     }
+        //     
+        // }
+        //
+        // public Type[] types
+        // {
+        //     get
+        //     {
+        //         if (IsNull()) return null;
+        //         Type[] copy = new Type[_names.Length];
+        //         Array.Copy(_types, copy, copy.Length);
+        //         return copy;
+        //     }
+        // }
+
+        public string[] Names { get; }
+
+        public Type[] Types { get; }
+
+        // public Type this[int index]
+        // {
+        //     get => Types[index];
+        // }
+
+        public string this[int index]
+        {
+            set
+            {
+                Types[index] = ModulaUtilities.GetTypeByName<TParentClass>(value);
+                Names[index] = value;
+            }
+        }
+
+        public bool IsNull()
+        {
+            return Types == null;
+        }
+
+        public string GetName(int index)
+        {
+            return Names[index];
+        }
+
+        public Type GetType(int index)
+        {
+            return Types[index];
+        }
+
+        public Type GetType(string name)
+        {
+            return Types[Array.IndexOf(Names, name)];
+        }
+    }
+}
