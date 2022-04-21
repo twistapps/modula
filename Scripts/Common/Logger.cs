77f31e704e1416e0e21cc4f5612b01b4d5353894
bloomstick <twistappscontact@gmail.com>
2022-04-22 00:42:15 +0300

    feat: logging errors with Logger

--- Scripts/Common/Logger.cs
+++ Scripts/Common/Logger.cs
@@ -1,7 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Modula.Common
 {
+    public enum ErrorCode
+    {
+        __EMPTY,
+        MissingModule,
+        PropertyNULL
+    }
+    
     public class Logger
     {
         
@@ -31,6 +39,40 @@ namespace Modula.Common
                 ", reason: ", GetModuleRemoveReasonSlug(reason),
                 " (class ", obj.GetType().Name, ")");
             Debug.Log(message, obj);
+        }
+
+        private static string GetErrorMessageByCode(ErrorCode code, GameObject obj, string context)
+        {
+            string msg;
+            switch (code)
+            {
+                case ErrorCode.__EMPTY:
+                default:
+                    msg = "Unhandled error.";
+                    break;
+                
+                case ErrorCode.MissingModule:
+                    msg = "Missing Module";
+                    break;
+                case ErrorCode.PropertyNULL:
+                    msg = "Required Property is NULL";
+                    break;
+            }
+            
+            if (obj != null) msg += string.Concat("(on GameObject:", obj.name, ")");
+            if (context != null) msg += string.Concat(": ", context);
+            return msg;
+        }
+
+        public static void LogError(ErrorCode code, GameObject obj, string context = null, bool shouldLog = true)
+        {
+            if (!shouldLog) return;
+            Debug.LogError(GetErrorMessageByCode(code, obj, context), obj);
+        }
+
+        public static void LogError(ErrorCode code, GameObject obj, Type context = null, bool shouldLog = true)
+        {
+            LogError(code, obj, context?.Name, shouldLog);
         }
     }
 }
